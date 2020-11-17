46f22c093bb300fc5fd0f3fcd2f7c7551c493ba9
Elliott Porter <elliottp731@gmail.com>
2020-11-16 23:12:15 -0500

    Created Application class for Dagger-Hilt, an application module that provides the application context and Glide instance, and added name of Application class into AndroidManifest.xml

---
+++ app/src/main/java/com/elliottco/socialnetworkclone/di/AppModule.kt
@@ -0,0 +1,39 @@
+package com.elliottco.socialnetworkclone.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.elliottco.socialnetworkclone.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+/**
+ * Provides dependencies that live as long as the application does
+ */
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplicationContext(
+        @ApplicationContext context: Context
+    ) = context
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_placeholder_image)
+            .error(R.drawable.ic_error)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+}
