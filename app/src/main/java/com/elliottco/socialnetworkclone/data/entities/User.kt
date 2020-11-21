9eab3b9fc66083d2214a8f1f7fb4ad0020f53850
Elliott Porter <elliottp731@gmail.com>
2020-11-21 14:48:06 -0500

    Create User data class and got reference to Firebase Storage default user image

---
+++ app/src/main/java/com/elliottco/socialnetworkclone/data/entities/User.kt
@@ -0,0 +1,17 @@
+package com.elliottco.socialnetworkclone.data.entities
+
+import com.elliottco.socialnetworkclone.misc.Constants.DEFAULT_PROFILE_PICTURE_URL
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class User(
+    val uid: String = "",
+    val username: String = "",
+    val profilePictureUrl: String = DEFAULT_PROFILE_PICTURE_URL,
+    val description: String = "",
+    var follows: List<String> = listOf(),
+
+    @Exclude
+    var isFollowing: Boolean = false
+)
---
+++ app/src/main/java/com/elliottco/socialnetworkclone/misc/Constants.kt
@@ -0,0 +1,6 @@
+package com.elliottco.socialnetworkclone.misc
+
+object Constants {
+
+    const val  DEFAULT_PROFILE_PICTURE_URL = "https://firebasestorage.googleapis.com/v0/b/socialnetworkclone-8fd90.appspot.com/o/default_profile_picture.png?alt=media&token=7a00df64-c008-4f5a-a957-a8e49bf453fb"
+}
