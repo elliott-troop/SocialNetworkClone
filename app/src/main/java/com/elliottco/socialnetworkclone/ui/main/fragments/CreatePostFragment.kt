4834ab175c5ccfb20eb9e7c211efc55f9a54b938
Elliott Troop <elliottp731@gmail.com>
2020-11-29 12:29:42 -0500

    Observe post status from viewmodel in CreatePostFragment.kt

--- app/src/main/java/com/elliottco/socialnetworkclone/ui/main/fragments/CreatePostFragment.kt
+++ app/src/main/java/com/elliottco/socialnetworkclone/ui/main/fragments/CreatePostFragment.kt
@@ -5,11 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.elliottco.socialnetworkclone.R
+import com.elliottco.socialnetworkclone.misc.EventObserver
+import com.elliottco.socialnetworkclone.ui.main.viewmodels.CreatePostViewModel
+import com.elliottco.socialnetworkclone.ui.snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_create_post.*
 
 @AndroidEntryPoint
 class CreatePostFragment : Fragment() {
+
+    private val viewModel: CreatePostViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +31,25 @@ class CreatePostFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_post, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeToObservers()
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.createPostStatus.observe(viewLifecycleOwner, EventObserver(
+                onError = {
+                    createPostProgressBar.isVisible = false
+                    snackbar(it)
+                },
+                onLoading = {
+                    createPostProgressBar.isVisible = true
+                }
+        ){
+            createPostProgressBar.isVisible = false
+            findNavController().popBackStack()
+        })
     }
 }
