eafa23da7bcddbece40933fa9086b1f00c4c54eb
Elliott Troop <elliottp731@gmail.com>
2020-11-29 00:57:21 -0500

    Added check for user login state in AuthActivity.kt

--- app/src/main/java/com/elliottco/socialnetworkclone/AuthActivity.kt
+++ app/src/main/java/com/elliottco/socialnetworkclone/AuthActivity.kt
@@ -1,7 +1,10 @@
 package com.elliottco.socialnetworkclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.elliottco.socialnetworkclone.ui.main.MainActivity
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +12,13 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        // Check to see if a user is still logged in. If so, bring them directly to the MainActivity
+        if(FirebaseAuth.getInstance().currentUser != null) {
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
     }
 }
